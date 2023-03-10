4834ee5193e461dd1ae750d7c20234db4e0ae6c9
rfs1998 <61716210+rfs1998@users.noreply.github.com>
2023-03-10 18:15:49 -0500

    fix grid size

--- GameOfLife/MainWindow.cpp
+++ GameOfLife/MainWindow.cpp
@@ -2,7 +2,7 @@
 
 
 MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Sample Title",
-	wxPoint(0,0), wxSize(1000,1000)) {
+	wxPoint(0,0), wxSize(400,400)) {
 	p_drawingPanel = new DrawingPanel(this);
 }
 
